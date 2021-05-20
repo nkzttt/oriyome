6f055c8825d82d6d171e5f233e492cb890a8c75e
hiroki1nakazato <nakazato.imt@gmail.com>
2021-05-20 17:20:19 +0900

    added the ress.

--- _sources/src/App.tsx
+++ _sources/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import 'ress';
 
 function App() {
-  return <div>App</div>
+  return <div>App</div>;
 }
 
-export default App
+export default App;
