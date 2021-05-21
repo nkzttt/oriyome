2ece772736d3cd0e0d2e0d7c5e83eb078ac493ad
hiroki1nakazato <nakazato.imt@gmail.com>
2021-05-22 00:46:02 +0900

    created global styles by the styled-components.

--- _sources/src/App.tsx
+++ _sources/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import 'ress';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 import ChooseCharacter from './components/chooseCharacter/ChooseCharacter';
 
-const App: React.FC = () => <ChooseCharacter />;
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family:"ヒラギノ丸ゴ Pro W4","ヒラギノ丸ゴ Pro","Hiragino Maru Gothic Pro","ヒラギノ角ゴ Pro W3","Hiragino Kaku Gothic Pro","HG丸ｺﾞｼｯｸM-PRO","HGMaruGothicMPRO";
+    background-color: #f7f7f7;
+    color: #646464;
+  }
+`;
+
+const App: React.FC = () => (
+  <div>
+    <GlobalStyle />
+    <ChooseCharacter />
+  </div>
+);
 
 export default App;
